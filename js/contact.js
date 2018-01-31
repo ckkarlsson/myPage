5178b8353552083160eebd4bd86215e7bb00ba58
Your Name <you@example.com>
2018-01-31 23:02:13 +0100

    First commit

---
+++ js/contact.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    console.log( "ready!" );
+
+
+/*$("#animation").effect("bounce", { times:3 }, 300);*/
+
+});
---
+++ js/main.js
@@ -0,0 +1,43 @@
+function neonGo() {
+    var el = document.getElementById("styleh1");    /*.style.color = "blue";*/
+    el.style.color = "#FFF";
+    el.style.textShadow = "0 0 5px #fff, 0 0 10px #fff, 0 0 20px #ee42f4, 0 0 30px #ee42f4, 0 0 40px #ee42f4, 0 0 55px #ee42f4, 0 0 65px #ee42f4";
+
+    var eltwo = document.getElementById("background");
+    eltwo.style.backgroundColor = "black"; 
+
+    var elthree = document.getElementById("styleNav1");
+    elthree.style.color = "#fff";
+    elthree.style.textShadow = "0 0 5px #fff, 0 0 10px #fff, 0 0 20px #ee42f4, 0 0 30px #ee42f4, 0 0 55px #ee42f4";
+
+    var elthree2 = document.getElementById("styleNav2");
+    elthree2.style.color = "#fff";
+    elthree2.style.textShadow = "0 0 5px #fff, 0 0 10px #fff, 0 0 20px #ee42f4, 0 0 30px #ee42f4, 0 0 55px #ee42f4";
+
+    var elthree3 = document.getElementById("styleNav3");
+    elthree3.style.color = "#fff";
+    elthree3.style.textShadow = "0 0 5px #fff, 0 0 10px #fff, 0 0 20px #ee42f4, 0 0 30px #ee42f4, 0 0 55px #ee42f4";
+
+    var elthree4 = document.getElementById("styleNav4");
+    elthree4.style.color = "#fff";
+    elthree4.style.textShadow = "0 0 5px #fff, 0 0 10px #fff, 0 0 20px #ee42f4, 0 0 30px #ee42f4, 0 0 55px #ee42f4";
+
+    document.getElementById("buttonId").innerText = "What did you do?! Turn back!";
+    var elfour = document.getElementById("buttonId");
+    elfour.style.color = "red";
+    elfour.style.fontSize = "23px";
+    elfour.style.fontWeight = "900";
+    elfour.style.backgroundImage = "url(smiley.jpg)";
+    elfour.style.boxShadow = "0 8px yellow";
+
+    document.getElementById("imgDeco").src = "http://www.reactiongifs.com/r/2012/11/chicken_regret_nothing.gif";
+
+    var elfive1 = document.getElementById("footer1");
+    elfive1.style.color = "#fff";
+    elfive1.style.textShadow = "0 0 5px #fff, 0 0 10px #fff, 0 0 20px #ee42f4, 0 0 30px #ee42f4, 0 0 55px #ee42f4";
+
+    var elfive2 = document.getElementById("footer2");
+    elfive2.style.color = "#fff";
+    elfive2.style.textShadow = "0 0 5px #fff, 0 0 10px #fff, 0 0 20px #ee42f4, 0 0 30px #ee42f4, 0 0 55px #ee42f4";
+
+}
